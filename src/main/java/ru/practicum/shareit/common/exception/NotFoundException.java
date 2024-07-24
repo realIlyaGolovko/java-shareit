8fdbac28290fee914861a8e142d190ab10a7253b
Ilya Golovko <i.i.golovko@tinkoff.ru>
2024-07-24 09:08:22 +0300

    fix: delete toString for exception

--- src/main/java/ru/practicum/shareit/common/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/common/exception/NotFoundException.java
@@ -1,8 +1,5 @@
 package ru.practicum.shareit.common.exception;
 
-import lombok.ToString;
-
-@ToString
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
